078b07c677dfbc87f936ffccc5832378097d1fa7
Petermik6430 <167068439+Petermik6430@users.noreply.github.com>
2025-02-14 13:04:14 +0100

    Excersice Generic Generic 2 Generic types

---
+++ Exercise_Generics/Generic2-GenericVersion/Manager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise_Generics.Generic2_NonGeneric;
+using Exercise_Generics.Generic2_NonGeneric.Model;
+
+namespace Exercise_Generics.Generic2_GenericVersion
+{
+    internal class Manager
+    {
+        public static void Main(string[] args)
+        {
+            OrderedPair<String> fruit = new OrderedPair<String>("apple", "banana");
+            Console.WriteLine("Order of strings");
+            Console.WriteLine(fruit);
+            fruit.ChangeOrder();
+            Console.WriteLine("Order changed!");
+            Console.WriteLine(fruit);
+            // OrderedPair type int
+            OrderedPair<int> numbers = new OrderedPair<int>(9, 11);
+            Console.WriteLine(Environment.NewLine + "Order of integers");
+            Console.WriteLine(numbers);
+            numbers.ChangeOrder();
+            Console.WriteLine("Order changed!");
+            Console.WriteLine(numbers);
+            // OrderedPair type City
+            City aCity = new City("Nuuk", 40000);
+            City bCity = new City("Thorshavn", 20000);
+            OrderedPair<City> cities = new OrderedPair<City>(aCity, bCity);
+            Console.WriteLine(Environment.NewLine + "Order of cities");
+            Console.WriteLine(cities);
+            cities.ChangeOrder();
+            Console.WriteLine("Order changed!");
+            Console.WriteLine(cities);
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Exercise_Generics/Generic2-GenericVersion/OrderedPair.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise_Generics.Generic2_NonGeneric.Model;
+
+namespace Exercise_Generics.Generic2_GenericVersion
+{
+    internal class OrderedPair<T> : IPairable<T>
+    {
+        public OrderedPair(T first, T second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public void ChangeOrder()
+        {
+            T temp = First;
+            First = Second;
+            Second = temp;
+        }
+
+        public T First { get; set; }
+        public T Second { get; set; }
+
+        public override string ToString()
+        {
+            return $"First: {First} \nSecond: {Second}";
+        }
+    }
+}
