f09ad774b3387714fdfd2839148d216ce8839609
Petermik6430 <167068439+Petermik6430@users.noreply.github.com>
2025-03-02 12:29:41 +0100

    FirstC#Project

---
+++ Firstc#/Hello.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.String;
+
+namespace Firstc_
+{
+    class Hello
+    {
+        public string? Name { get; set; }
+        public Hello(string name)
+        {
+            Name = name;
+        }
+
+        public override string? ToString()
+        {
+            string str = "Hello " + Name;
+            if (!IsNullOrWhiteSpace(str))
+            {
+                return "Error no Input";
+            }
+            return str;
+        }
+    }
+}
---
+++ Firstc#/Program.cs
@@ -0,0 +1,12 @@
+﻿using Firstc_;
+
+Hello hello = new Hello("Hans");
+
+Hello hello1 = new Hello(null);
+
+Hello hello2 = new Hello("Succes");
+Console.WriteLine($"Hello, {hello.Name}! \n");
+Console.WriteLine("Should be no input");
+Console.WriteLine(hello1 + "\n");
+Console.WriteLine("Should be no input");
+Console.WriteLine(hello2 + "\n");
