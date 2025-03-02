1f3f39536ca472844d70b571f1b609ceab258f43
Petermik6430 <167068439+Petermik6430@users.noreply.github.com>
2025-03-02 14:13:53 +0100

    Ekstra

---
+++ IEnumerable/AList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable
+{
+    class AList
+    {
+
+        public IEnumerable<string?>? SomeStrings { get; set; }
+
+        public AList ()
+        {
+            SomeStrings = new List<string?> { null, "Hello", "Hans", "Hansen" };
+        }
+
+
+
+        public override string? ToString()
+        {
+            string str = $"{SomeStrings}";
+                return str;
+        }
+    }
+}
---
+++ IEnumerable/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Linq;
+
+namespace IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AList listOfWord = new AList();
+            List<string> numberOfEntries = new List<string>();
+            int characters = 0;
+
+
+            foreach (var word in listOfWord.SomeStrings)
+            {
+                if (string.IsNullOrWhiteSpace(word) || listOfWord == null)
+                {
+                    Console.WriteLine("Error no Input");
+
+                }
+                else
+                {
+                    Console.WriteLine(word);
+                    numberOfEntries.Add(word);
+                    characters += word.Length;
+
+                }
+
+            }
+            Console.WriteLine( $"Numbers of entries: {numberOfEntries.Count}");
+            //Console.WriteLine($"Numbers of entries {listOfWord.SomeStrings.Count()}");
+            Console.WriteLine($"Numbers of entries: {characters}");
+
+
+        }   
+        
+    }
+}
+
+
+
