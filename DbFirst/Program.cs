41e6e5abe0608c7b85cc4b939a77f3f76c96c252
Petermik6430 <10526043@ucn.dk>
2025-03-23 11:09:00 +0100

    opgave dbfirst

---
+++ DbFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+
+
+namespace DbFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+            using (var conn= new SqlConnection(connection))
+            {
+                connection.Open();
+                Console.WriteLine("Connected to database.");
+            }
+        }
+            
+       
+    }
+}
